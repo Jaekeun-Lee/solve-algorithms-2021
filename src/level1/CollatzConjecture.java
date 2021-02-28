02e2321fe826e0235e206386fff1ad77c0645ed7
Jaekeun Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2021-02-28 17:18:07 +0900

    Initial commit

---
+++ src/level1/CollatzConjecture.java
@@ -0,0 +1,27 @@
+package level1;
+
+public class CollatzConjecture {
+
+    public int solution(long num) {
+        int repeatCnt = 0;
+
+        while (num != 1) {
+
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = (num * 3) + 1;
+            }
+
+            repeatCnt++;
+            if(repeatCnt>=500) return -1;
+        }
+
+        return repeatCnt;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new CollatzConjecture().solution(626331));
+    }
+}
