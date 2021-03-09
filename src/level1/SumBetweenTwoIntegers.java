01e39e46b745a3cb4d6c50297bd978a970b28bae
Jaekeun Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2021-03-09 22:17:54 +0900

    create SumBetweenTwoIntegers.java

---
+++ src/level1/SumBetweenTwoIntegers.java
@@ -0,0 +1,21 @@
+package level1;
+
+public class SumBetweenTwoIntegers {
+
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        if (a != b) {
+            for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+                answer += i;
+            }
+        } else {
+            answer = a;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SumBetweenTwoIntegers().solution(3, 5));
+    }
+}
